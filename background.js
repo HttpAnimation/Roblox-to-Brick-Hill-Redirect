88d7f2282acb51b08d928ff5dece3738dab1f754
HttpAnimations <97435656+HttpAnimation@users.noreply.github.com>
2023-05-09 14:27:48 -0700

    Update background.js

--- background.js
+++ background.js
@@ -1,6 +1,6 @@
 const redirectUrl = "https://www.brick-hill.com/dashboard";
 
-chrome.webRequest.onBeforeRequest.addListener(
+browser.webRequest.onBeforeRequest.addListener(
   function(details) {
     if (details.url.includes("roblox.com")) {
       return {redirectUrl: redirectUrl};
